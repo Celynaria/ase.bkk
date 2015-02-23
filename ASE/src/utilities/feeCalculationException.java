39de908ce582c0624ae9c23f71810d6f98dc4401
thanaphong sontayasara <ts5@hw.ac.uk>
2015-02-23 18:01:06 +0000

    exception

---
+++ ASE/src/utilities/feeCalculationException.java
@@ -0,0 +1,32 @@
+package utilities;
+
+public class feeCalculationException extends Exception{
+
+	public feeCalculationException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public feeCalculationException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public feeCalculationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public feeCalculationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public feeCalculationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
