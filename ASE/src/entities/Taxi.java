e0081d8878796f64bacc480f31ee4c9b40bed566
Wanchana Ekakkharanon <wonchana@yahoo.com>
2015-02-24 10:35:22 +0000

    delete extra import class

--- ASE/src/entities/Taxi.java
+++ ASE/src/entities/Taxi.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import utilities.InvalidRegistrationFormatException;
 /**
