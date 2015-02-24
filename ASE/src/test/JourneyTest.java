3ad9f308fa990598f8f9240ec6a73198e14fe089
Wanchana Ekakkharanon <wonchana@yahoo.com>
2015-02-24 13:05:02 +0000

    Update more test

--- ASE/src/test/JourneyTest.java
+++ ASE/src/test/JourneyTest.java
@@ -2,13 +2,48 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class JourneyTest {
+import utilities.FeeCalculationException;
+import entities.Destination;
+import entities.Journey;
 
+public class JourneyTest {
+	private List<double[]> inputList = new ArrayList<double[]>();
+	@Before
+	public void prepareData(){
+		double[] a = {5,70.7};
+		double[] b = {4,70.7};
+		double[] c = {3,65.7};
+		this.inputList.add(a);
+		this.inputList.add(b);
+		this.inputList.add(c);
+	}
+
+	@SuppressWarnings("deprecation")
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFeeCalculation() {
+		Journey journey = new Journey();
+		Destination des = new Destination();
+		des.setDistance(65.7);
+		journey.setDestination(des);
+		Iterator<double[]> i = this.inputList.iterator();
+		while(i.hasNext()){
+			double[] input = i.next();
+			journey.setNumberOfPassenger(Integer.parseInt(Double.toString(input[0])));
+			try {
+				assertEquals(input[1], journey.feeCalculation());
+			} catch (FeeCalculationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
