12ca10ad8c7062a7f94e2bd31194969900722c11
Wanchana Ekakkharanon <wonchana@yahoo.com>
2015-02-24 13:35:05 +0000

    add comment

--- ASE/src/test/TaxiTest.java
+++ ASE/src/test/TaxiTest.java
@@ -6,6 +6,12 @@ import org.junit.Test;
 import entities.Taxi;
 import utilities.InvalidRegistrationFormatException;
 
+/**
+ * Simple unit test for checking Taxi.registrationIDChecker() method inside Taxi class.
+ * This test will expected a matched two custom exception messages .
+ * @author Wanchana, Thanaphong
+ *
+ */
 public class TaxiTest {
 	@Test
 	public void registrationIDFormatTest(String ID) {
@@ -22,7 +28,7 @@ public class TaxiTest {
 			Taxi.registrationIDChecker(ID);
 			Assert.fail("There are no exception");
 		} catch(InvalidRegistrationFormatException e) {
-			Assert.assertFalse(e.getMessage().equals("The length of RegistrationID must be 8 digits"));
+			Assert.assertTrue(e.getMessage().equals("The length of RegistrationID must be 8 digits"));
 		}
 	}
 
