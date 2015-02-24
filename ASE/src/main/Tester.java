2e7e048a7c1d8613f71001e1e92e72367669d4ae
Wanchana Ekakkharanon <wonchana@yahoo.com>
2015-02-24 13:04:38 +0000

    tester class

--- ASE/src/main/Tester.java
+++ ASE/src/main/Tester.java
@@ -1,12 +1,15 @@
 package main;
 
+import test.JourneyTest;
 import test.TaxiTest;
 
 public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TaxiTest test = new TaxiTest();
-		test.registrationIDLengthTest("BKKAAA01");
+//		TaxiTest test = new TaxiTest();
+//		test.registrationIDLengthTest("BKKAAA01");
+		JourneyTest test = new JourneyTest();
+		test.testFeeCalculation();
 	}
 }
